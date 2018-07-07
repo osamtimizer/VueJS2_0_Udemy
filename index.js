d5aed7b52ea24dc9da01f9a20b6695dbaa53aaf6
osamtimizer <eaten.jb@gmail.com>
2018-07-07 23:56:41 +0900

    2-16, add click event listener using v-on

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'hello world',
-    link: "http://google.com",
-    finishedLink: '<a href="http://google.com">Google</a>'
+    counter: 0
   },
-  methods: {
-    changeTitle: function (event) {
-      this.title = event.target.value;
-    },
-    sayHello: function(){
-      return this.title;
+  methods:{
+    increase: function(){
+      this.counter++;
     }
   }
 });
