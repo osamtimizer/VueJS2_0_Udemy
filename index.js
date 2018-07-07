37e5b26f2a9d82d03aa710f6426af6b18282f376
osamtimizer <eaten.jb@gmail.com>
2018-07-07 23:21:15 +0900

    2-13 add a data for bindings.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'hello world'
+    title: 'hello world',
+    link: "http://google.com"
   },
   methods: {
     changeTitle: function (event) {
       this.title = event.target.value;
     },
-    //Actually, "this" is not referring "data",
-    //however, Vue.js is proxying and handling between data and methods,
-    //therefore you can access the data you desired.
     sayHello: function(){
       return this.title;
     }
